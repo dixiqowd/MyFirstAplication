d7ca6705c5afcc89077c9a271c84d9f165f26a4c
yzz <963372120@qq.com>
2023-10-09 00:31:13 +0800

    增加注释

--- src/main/java/com/example/webhomework/WebhomeWorkApplication.java
+++ src/main/java/com/example/webhomework/WebhomeWorkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebhomeWorkApplication {
 
     public static void main(String[] args) {
+        //这里是启动代码
         SpringApplication.run(WebhomeWorkApplication.class, args);
     }
 /**
